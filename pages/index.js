8dcd4bf18a9de524f9936ba89f4dcd134985f518
Pedro Vaz <pedro0vazs@gmail.com>
2021-07-15 15:20:44 -0300

    useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,61 @@
+import React from 'react';
+
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 
-function ProfileSideBar(props) {
+function ProfileSideBar(propriedades) {
   
   return (
-    <Box>
-      <img src={`https://github.com/${props.gitHubUser}.png`} style={{ borderRadius: '8px' }}/>
+    <Box as='aside'>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }}/>
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.githubUser}`} target="blank">
+          @{propriedades.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault githubUser={propriedades.githubUser} />
     </Box>
   )
 };
 
+function ProfileRelationsBox(propriedades) {
+  return (
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {propriedades.title} ({propriedades.items.length})
+      </h2>
+      {/* <ul>
+        {seguidores.map(itemAtual => {
+          return (
+            <li key={itemAtual}>
+              <a href={`/users/${itemAtual}`}>
+                <img src={`https://github.com/${itemAtual}.png`} style={{ borderRadius: '8px'}} />
+                <span>{itemAtual}</span>
+              </a>
+            </li>
+          )
+        })}
+      </ul> */}
+    </ProfileRelationsBoxWrapper>
+  )
+};
+
 export default function Home() {
-  const gitHubUser = 'pedrovazs';
+  const githubUser = 'pedrovazs';
+  
+  const [ comunidades, setComunidades ] = React.useState([{
+    id: '135465321321321',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
+  
   const pessoasFavoritas = [
     'marcobrunodev',
     'rafaballerini',
@@ -24,32 +65,101 @@ export default function Home() {
     'felipefialho'
   ];  
 
+  const [ seguidores, setSeguidores ] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://api.github.com/users/rafaballerini/followers')
+      .then( respostaDoServidor => {
+        return respostaDoServidor.json();
+      })
+      .then( respostaCompleta => {
+        setSeguidores(respostaCompleta);
+      })
+  }, [])
+
+
+
   return (
     <>
       <AlurakutMenu />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <ProfileSideBar gitHubUser={gitHubUser} />
+          <ProfileSideBar githubUser={githubUser} />
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
             <h1 className="title">
               Bem-Vindo(a)
             </h1>
-
             <OrkutNostalgicIconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer? </h2>
+            <form
+              onSubmit={function handleCriaComiunidade(e){
+                e.preventDefault();
+                const dadosForm = FormData(e.target);
+
+                const comunidade = {
+                  id: new Date().toISOString,
+                  title: dadosForm.get('title'),
+                  image: dadosForm.get('image')
+                };
+
+                const ComunidadesAtualizadas = [...comunidades, comunidade]
+                setComunidades(ComunidadesAtualizadas);
+            }}
+            >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBox title="Seguidores" items={seguidores}/>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
-              Favoritos ({pessoasFavoritas.length})
+              Comunidades ({comunidades.length})
+            </h2>
+              <ul>
+              {comunidades.map(itemAtual => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} style={{ borderRadius: '8px'}} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas Favoritas ({pessoasFavoritas.length})
             </h2>
             <ul>
               {pessoasFavoritas.map(itemAtual => {
                 return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} style={{ borderRadius: '8px'}} />
                       <span>{itemAtual}</span>
                     </a>
@@ -58,9 +168,6 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>
-            Comunidades
-          </Box>
         </div>
       </MainGrid>
     </>
